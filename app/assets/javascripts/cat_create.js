0937052d77fbd6b1480cdc078d15ad1a93a08df4
Przemek <hello@krowinski.com>
2015-03-02 01:41:31 +0100

    Prepare basic cat creation

--- app/assets/javascripts/cat_create.js
+++ app/assets/javascripts/cat_create.js
@@ -1,30 +1,34 @@
-function change_setting(setting_name, by) {
+function change_setting(setting_name, by, limit) {
   var current_value = parseInt($(".selected-"+setting_name).val());
   current_value += by;
   if(current_value < 0) {
-    current_value = 9;
-  } else if(current_value > 9) {
+    current_value = limit;
+  } else if(current_value > limit) {
     current_value = 0;
   }
 
   $(".setting-image-"+setting_name).attr("src", "/public_images/appearance/"+setting_name+"/"+current_value+".png");
 
   $(".selected-"+setting_name).val(current_value);
-
-  alert(current_value);
 }
 
 $(document).ready(function(){
   $(".setting-eyes .btn-left").click(function(){
-    change_setting("eyes", -1);
+    change_setting("eyes", -1, 9);
   });
   $(".setting-eyes .btn-right").click(function(){
-    change_setting("eyes", 1);
+    change_setting("eyes", 1, 9);
   });
   $(".setting-fur .btn-left").click(function(){
-    change_setting("fur", -1);
+    change_setting("fur", -1, 9);
   });
   $(".setting-fur .btn-right").click(function(){
-    change_setting("fur", 1);
+    change_setting("fur", 1, 9);
+  });
+  $(".setting-face .btn-left").click(function(){
+    change_setting("face", -1, 2);
+  });
+  $(".setting-face .btn-right").click(function(){
+    change_setting("face", 1, 2);
   });
 });
