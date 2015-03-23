45534fb2b7d88b9d85b7463ed834e7d408307d17
Przemek <przemek@krowinski.eu>
2015-03-23 02:34:51 +0100

    Remove email options from devise and add uniqueness

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :registerable, :rememberable, :trackable, :validatable
+
+  validates_uniqueness_of :name
 
   has_many :user_cats
 
