196e51606b58a3c1a44dc0f1071949cd47257903
Przemek <przemek@krowinski.eu>
2015-03-23 02:32:00 +0100

    Add present method for automatically applying presenters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,15 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  private
+
+  def present(target_object)
+    presenter_class_name = "#{target_object.class}Presenter"
+    presenter_class_name.constantize.new(target_object)
+  rescue NameError => e
+    target_object
+  end
+
   def current_cat
     current_user.user_cats.first
   end
