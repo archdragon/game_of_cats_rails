4a884be94b9f0d602c27c63c5b9f26bd6b14329b
Przemek <przemek@krowinski.eu>
2015-03-23 02:32:37 +0100

    Add visit '/'

--- spec/features/guest/browse_index_spec.rb
+++ spec/features/guest/browse_index_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 feature 'user browses the main page as a guest' do
   scenario 'user sees the cat creation' do
+    visit '/'
     expect(page).to have_css('#cat-create')
   end
 end
