fe56c4ad118a8769e13e06bb887aa4d7e32a8013
Przemek <przemek@krowinski.eu>
2015-03-26 03:00:47 +0100

    Add cat attributes and user gold

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150321232416) do
+ActiveRecord::Schema.define(version: 20150325220655) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,9 @@ ActiveRecord::Schema.define(version: 20150321232416) do
     t.integer  "interest3",  default: 0
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "attribute1", default: 1
+    t.integer  "attribute2", default: 1
+    t.integer  "attribute3", default: 1
   end
 
   create_table "users", force: :cascade do |t|
@@ -44,6 +47,7 @@ ActiveRecord::Schema.define(version: 20150321232416) do
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
+    t.integer  "gold",                   default: 1
   end
 
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
