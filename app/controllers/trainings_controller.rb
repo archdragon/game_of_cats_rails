b754611a7f67d7dc1e64828d48f47fe83d4dba0a
Przemek <przemek@krowinski.eu>
2015-03-26 03:03:57 +0100

    Add training data loading

---
+++ app/controllers/trainings_controller.rb
@@ -0,0 +1,12 @@
+class TrainingsController < ApplicationController
+  layout "logged_in"
+  def index
+    @trainings = Training.all
+    @trainings.map! { |training| CatTraining.new(cat: current_cat, training: training) }
+  end
+
+  def start
+    response = StartTrainingService.call(cat: current_cat, training_id: params[:id], user: current_user)
+    redirect_to trainings_path, flash: response.flash
+  end
+end
