0e4c348b96998bd518a363caed043622af9618d9
Przemek <przemek@krowinski.eu>
2015-03-26 03:06:04 +0100

    Add a Service Object for changing cat energy

---
+++ app/services/change_cat_energy_service.rb
@@ -0,0 +1,9 @@
+class ChangeCatEnergyService
+  def self.call(cat:, energy_change:)
+    new_amount = cat.energy + energy_change
+    raise ServiceError.new("Not enough energy!") if new_amount < 0
+
+    cat.update_attributes!(energy: new_amount)
+    ServiceResponse.new(message: "Cat energy changed!", success: true)
+  end
+end
