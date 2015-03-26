a981cd3e189e3a6fd2554d976d6c767042e1667f
Przemek <przemek@krowinski.eu>
2015-03-26 03:03:22 +0100

    Add flash message class translator

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,17 @@ module ApplicationHelper
   def devise_mapping
     @devise_mapping ||= Devise.mappings[:user]
   end
+
+  def flash_class(class_name)
+    case class_name
+    when 'notice'
+      'alert-info'
+    else
+      class_name
+    end
+  end
+
+  def current_cat
+    current_user.current_cat
+  end
 end
