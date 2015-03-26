15cdca7374bb63e870580eaba91b2a93f6109a24
Przemek <przemek@krowinski.eu>
2015-03-26 02:59:01 +0100

    Move present function to a separate class

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,16 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  private
-
-  def present(target_object)
-    presenter_class_name = "#{target_object.class}Presenter"
-    presenter_class_name.constantize.new(target_object)
-  rescue NameError => e
-    target_object
-  end
-
   def current_cat
-    current_user.user_cats.first
+    current_user.current_cat
   end
 end
