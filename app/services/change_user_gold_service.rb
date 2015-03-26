97f757aeb95f2e62731af3bd1a29902b53d7a660
Przemek <przemek@krowinski.eu>
2015-03-26 03:06:19 +0100

    Add a class object for changing user gold

---
+++ app/services/change_user_gold_service.rb
@@ -0,0 +1,9 @@
+class ChangeUserGoldService
+  def self.call(user:, gold_change:)
+    new_amount = user.gold + gold_change
+    raise ServiceError.new("You don't have enough gold!") if new_amount < 0
+
+    user.update_attribute("gold", new_amount)
+    ServiceResponse.new(message: "Gold changed!", success: true)
+  end
+end
