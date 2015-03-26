c71396dffc2bd125401c6f0b47c8c906e6c051ce
Przemek <przemek@krowinski.eu>
2015-03-26 02:59:25 +0100

    Add mission loading and deorating

--- app/controllers/missions_controller.rb
+++ app/controllers/missions_controller.rb
@@ -1,5 +1,9 @@
 class MissionsController < ApplicationController
+  layout "logged_in"
   def index
+    @missions = Mission.all
+    @missions.map! { |mission| CatMission.new(cat: current_cat, mission: mission) }
+    logger.debug(@missions)
   end
   def start
   end
