c4318d8030ca90b86e7c241f841a5c7ec2a0797a
Przemek <przemek@krowinski.eu>
2015-04-01 17:39:25 +0200

    Add automatic language detection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,16 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  before_action :set_locale
   protect_from_forgery with: :exception
 
   def current_cat
     current_user.current_cat
+  end
+
+  private
+
+  def set_locale
+    I18n.locale = http_accept_language.compatible_language_from(I18n.available_locales)
   end
 end
