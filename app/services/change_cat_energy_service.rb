8f6b47cbebf16dd1eaeebde1beb8c43a10350e76
Przemek <przemek@krowinski.eu>
2015-04-01 17:43:08 +0200

    Cap max energy at 100

--- app/services/change_cat_energy_service.rb
+++ app/services/change_cat_energy_service.rb
@@ -1,7 +1,9 @@
 class ChangeCatEnergyService < ArchServiceObject
-  def self.call(cat:, energy_change:)
+  def self.body(cat:, energy_change:)
     new_amount = cat.energy + energy_change
     raise ServiceError.new("Not enough energy!") if new_amount < 0
+
+    new_amount = 100 if new_amount > 100
 
     cat.update_attributes!(energy: new_amount)
     ServiceResponse.new(message: "Cat energy changed!", success: true)
