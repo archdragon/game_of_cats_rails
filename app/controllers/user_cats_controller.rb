66576edaf4f2aee407104f2c72e74ae2f669b7f5
Przemek <przemek@krowinski.eu>
2015-04-01 17:41:26 +0200

    Add name generation

--- app/controllers/user_cats_controller.rb
+++ app/controllers/user_cats_controller.rb
@@ -4,4 +4,9 @@ class UserCatsController < ApplicationController
   def show
     @cat = ArchPresenter.present(current_cat)
   end
+
+  def generate_name
+    cat_name = GenerateCatNameService.call
+    render plain: cat_name
+  end
 end
