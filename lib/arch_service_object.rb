af409c3d8d0e60167178297ad038c8da2aafd299
Przemek <przemek@krowinski.eu>
2015-04-01 17:37:05 +0200

    Allow the objects to return exceptions or service messages

--- lib/arch_service_object.rb
+++ lib/arch_service_object.rb
@@ -1,8 +1,16 @@
 class ArchServiceObject
   def self.call(args)
-    ActiveRecord::Base.transaction { body(args) }
-  rescue ServiceError => e
-    ServiceResponse.new(message: e.message)
+    on_error = args.delete(:on_error) { :exception }
+
+    if on_error == :response_object
+      begin
+        ActiveRecord::Base.transaction { body(args) }
+      rescue ServiceError => e
+        ServiceResponse.new(message: e.message)
+      end
+    else
+      ActiveRecord::Base.transaction { body(args) }
+    end
   end
 
   def self.body(args)
