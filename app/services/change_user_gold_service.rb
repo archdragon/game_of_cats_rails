e7cef00221a167488fffe7ba7de3c0cc5a11345c
Przemek <przemek@krowinski.eu>
2015-04-02 03:12:27 +0200

    Use ArchServiceObject

--- app/services/change_user_gold_service.rb
+++ app/services/change_user_gold_service.rb
@@ -1,5 +1,5 @@
-class ChangeUserGoldService
-  def self.call(user:, gold_change:)
+class ChangeUserGoldService < ArchServiceObject
+  def self.body(user:, gold_change:)
     new_amount = user.gold + gold_change
     raise ServiceError.new("You don't have enough gold!") if new_amount < 0
 
