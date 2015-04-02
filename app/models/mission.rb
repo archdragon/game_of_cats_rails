85823b90fa13f7ab676e255cb5f18edde0ccfc95
Przemek <przemek@krowinski.eu>
2015-04-02 03:11:37 +0200

    Add mission gold

--- app/models/mission.rb
+++ app/models/mission.rb
@@ -1,4 +1,8 @@
 class Mission < ActiveYaml::Base
   set_root_path "#{Rails.root}/db/yaml"
-  fields :name, :id, :required_attributes
+  fields :name, :id, :required_attributes, :gold
+
+  def awarded_gold
+    rand(gold["from"].to_i..gold["to"].to_i)
+  end
 end
