1a9556147752eda11bee2c028e3b6a5412e00b0e
Przemek <przemek@krowinski.eu>
2015-04-02 03:11:48 +0200

    Add MAX_ENERGY

--- app/models/user_cat.rb
+++ app/models/user_cat.rb
@@ -6,6 +6,7 @@ class UserCat < ActiveRecord::Base
   belongs_to :user
 
   ACTION_COOLDOWN_IN_SECONDS = 15*60
+  MAX_ENERGY = 100
 
   def interest(n)
     self.send("interest#{n}")
@@ -18,5 +19,9 @@ class UserCat < ActiveRecord::Base
   def busy?
     last_action_time = last_action_at || Date.new(2000,1,1)
     (Time.now.to_i - last_action_time.to_time.to_i) < ACTION_COOLDOWN_IN_SECONDS
+  end
+
+  def max_energy
+    MAX_ENERGY
   end
 end
