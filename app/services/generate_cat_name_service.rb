ea99c2b45887c9092b8a4d3fea0133d19802acb7
Przemek <przemek@krowinski.eu>
2015-04-02 03:27:31 +0200

    Add a service for cat name generation
    * Use i18n data

---
+++ app/services/generate_cat_name_service.rb
@@ -0,0 +1,14 @@
+class GenerateCatNameService < ArchServiceObject
+  def self.call
+    cat_name = ""
+    if (1..7).to_a.sample == 1
+      cat_name += I18n.t('cat_names.prefixes').sample + " "
+    end
+
+    if (1..10).to_a.sample == 1
+      cat_name += I18n.t('cat_names.double_names.part1').sample + I18n.t('cat_names.double_names.part2').sample
+    else
+      cat_name += I18n.t('cat_names.singular_names').sample
+    end
+  end
+end
