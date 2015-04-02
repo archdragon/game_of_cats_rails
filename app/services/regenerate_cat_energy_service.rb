2f4792a852d44a71e996e8628694f024897830f5
Przemek <przemek@krowinski.eu>
2015-04-02 03:28:00 +0200

    Add a service responsible for regenerating cat energy

---
+++ app/services/regenerate_cat_energy_service.rb
@@ -0,0 +1,9 @@
+class RegenerateCatEnergyService < ArchServiceObject
+  ENERGY_CHANGE = rand(30..60)
+
+  def self.body(cat:)
+    raise ServiceError.new("Your cat can relax only once every 15 minutes!") if cat.busy?
+    cat.update_attributes(last_action_at: Time.now)
+    ChangeCatEnergyService.call(cat: cat, energy_change: ENERGY_CHANGE)
+  end
+end
