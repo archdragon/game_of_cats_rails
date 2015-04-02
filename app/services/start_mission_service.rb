7ca794e2bcc0f66e9adaa4d25d85acfd713fe8bd
Przemek <przemek@krowinski.eu>
2015-04-02 03:28:45 +0200

    Add a service responsible for initiating and finishing a cat mission

---
+++ app/services/start_mission_service.rb
@@ -0,0 +1,11 @@
+class StartMissionService < ArchServiceObject
+  ENERGY_CHANGE = -10
+
+  def self.body(user:, cat:, mission_id:)
+    mission = Mission.find(mission_id)
+    ChangeCatEnergyService.call(cat: cat, energy_change: ENERGY_CHANGE)
+    CheckCatAttributesService.call(cat: cat, required_attributes: mission.required_attributes)
+    ChangeUserGoldService.call(user: user, gold_change: mission.awarded_gold)
+    ServiceResponse.new(message: "Mission accomplished!", success: true)
+  end
+end
