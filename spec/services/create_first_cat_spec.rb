c2e77f8639401e46a48bca0c472f7f80f11ec612
Przemek <przemek@krowinski.eu>
2015-04-09 22:36:52 +0200

    Fix expected type

--- spec/services/create_first_cat_spec.rb
+++ spec/services/create_first_cat_spec.rb
@@ -4,10 +4,22 @@ RSpec.describe CreateFirstCatService do
   describe 'self.prepare_hobbies' do
     subject(:hobbies) { CreateFirstCatService.get_random_hobbies }
     it "Returns an array" do
-      expect(hobbies).to be_a(Hash)
+      expect(hobbies).to be_a(Array)
     end
     it "Contains 3 elements" do
       expect(hobbies.count).to eq(3)
+    end
+  end
+
+  describe 'self.call!' do
+    context "with persisted user" do
+      before do
+        @user = create(:user)
+        allow(@user).to receive_messages(:persisted? => true)
+      end
+      it "creates a cat" do
+        expect{CreateFirstCatService.call!(user: @user, cat_params: {})}.to change{UserCat.count}.by(1)
+      end
     end
   end
 end
