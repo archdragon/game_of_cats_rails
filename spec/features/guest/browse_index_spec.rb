24067a56bf5c944db6e9ef331b4c9bf26c3ddc68
Przemek <przemek@krowinski.eu>
2015-04-09 22:36:10 +0200

    Change feature description

--- spec/features/guest/browse_index_spec.rb
+++ spec/features/guest/browse_index_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-feature 'user browses the main page as a guest' do
+feature 'Browse index page' do
   scenario 'user sees the cat creation' do
     visit '/'
     expect(page).to have_css('#cat-create')
