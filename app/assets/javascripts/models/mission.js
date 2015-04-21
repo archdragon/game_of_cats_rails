6810dca56f6bf091d873b1545f0538f92b13b781
Przemek <przemek@krowinski.eu>
2015-04-22 00:56:55 +0200

    Add missions model

--- app/assets/javascripts/models/mission.js
+++ app/assets/javascripts/models/mission.js
@@ -1,3 +1,5 @@
-Cat.Mission  = DS.Model.extend({
-  name: DS.attr('string')
+Cat.Mission = DS.Model.extend({
+  name: DS.attr('string'),
+  translated_name: DS.attr('string'),
+  translated_description: DS.attr('string'),
 });
