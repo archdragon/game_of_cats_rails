82ccf9fa369c1c8e26e0ba257be56fc8bfd22246
Przemek <przemek@krowinski.eu>
2015-04-22 00:58:14 +0200

    Add regenerations route

--- app/assets/javascripts/routes/regenerations_route.js
+++ app/assets/javascripts/routes/regenerations_route.js
@@ -1,5 +1,8 @@
 Cat.RegenerationsRoute = Ember.Route.extend({
   model: function() {
     return this.store.find('regeneration');
+  },
+  setupController: function(controller, model) {
+    this.controller.set('model', model);
   }
 });
