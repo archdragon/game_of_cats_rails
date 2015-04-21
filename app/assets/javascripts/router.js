48460720d7d9f0fc2cd223cc4f53dd3f2f46b62c
Przemek <przemek@krowinski.eu>
2015-04-22 00:57:33 +0200

    Add basic router

--- app/assets/javascripts/router.js
+++ app/assets/javascripts/router.js
@@ -1,10 +1,10 @@
-// For more information see: http://emberjs.com/guides/routing/
-
 Cat.Router.map(function() {
-  // this.resource('posts');
   this.resource('about');
+  this.resource('currentUser');
   this.resource('cat');
   this.resource('trainings');
   this.resource('missions');
-  this.resource('regenerations');
+  this.resource('regenerations', function() {
+    this.route('start');
+  });
 });
