70cd3b1b47138f0f76f72fa2ad209be975625032
Przemek <przemek@krowinski.eu>
2015-04-25 00:04:52 +0200

    Use fetchById to always load fresh information about gold and energy

--- app/assets/javascripts/controllers/header_controller.js
+++ app/assets/javascripts/controllers/header_controller.js
@@ -1,17 +1,20 @@
 Cat.HeaderController = Ember.ObjectController.extend({
-  needs: ['currentUser', 'application', 'regenerations'],
+  needs: ['currentUser', 'application', 'regenerations', 'missions', 'trainings'],
   gold: 0,
   cat_energy: 0,
   preload: function() {
     this.refresh();
   }.on('init'),
   refresh: function() {
-    alert('Refreshing header');
     self = this;
-    currentUser = this.store.find('currentUser', 1).then(function (user) {
+    currentUser = this.store.fetchById('currentUser', 1).then(function (user) {
       self.set('gold', user.get('gold'));
       self.set('cat_energy', user.get('cat_energy'));
+      //alert('Update gold and energy: ' + self.get('gold') + ' ' + self.get('cat_energy'));
     });
-  }.observes('controllers.application.currentPath', 'controllers.regenerations.flashData')
+  }.observes('controllers.application.currentPath',
+    'controllers.regenerations.flashData.message',
+    'controllers.missions.flashData.message',
+    'controllers.trainings.flashData.message')
 
 });
