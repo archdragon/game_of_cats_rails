f7b84b7706dd923658c124a578a1266b1e101e72
Przemek <przemek@krowinski.eu>
2015-04-25 00:04:03 +0200

    Add a dynamic actionId

--- app/assets/javascripts/components/action_button.js
+++ app/assets/javascripts/components/action_button.js
@@ -1,5 +1,11 @@
 Cat.ActionButtonComponent = Ember.Component.extend({
   click: function() {
-    this.sendAction('action', 1);
+    this.sendAction('submit', this.get('actionId'));
+  },
+  mouseEnter: function() {
+    this.sendAction('on');
+  },
+  mouseLeave: function() {
+    this.sendAction('off');
   }
 });
